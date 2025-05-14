88e345ff819086bd4aeb1e10c51adf19956e249a
ALDCLAB <114551753+ALDCLAB@users.noreply.github.com>
2025-05-13 20:45:36 -0700

    fix

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -134,10 +134,10 @@ class RecipeCard extends HTMLElement {
       	<p class="title">
         	<a href="${data.titleLnk}">${data.titleTxt}</a>
       	</p>
-      <p class="organization">${data.organization}</p>
+      	<p class="organization">${data.organization}</p>
       	<div class="rating">
         	<span>${data.rating}</span>
-        	<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+        	<img src="./assets/images/icons/${data.rating}-star.svg" alt=" stars">
         	<span>(${data.numRatings})</span>
       	</div>
       	<time>${data.lengthTime}</time>
